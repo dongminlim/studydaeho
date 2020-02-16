3c391c40a72b559a78ca1b5558860336d1b9beb3
임동민 <koreadong88@mycelebs.com>
2020-02-16 16:36:11 +0900

    첫번째 과제 푸시

---
+++ assignmentProject/assignmentProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignmentProject
+//
+//  Created by 임동민 on 2020/02/16.
+//  Copyright © 2020 임동민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
